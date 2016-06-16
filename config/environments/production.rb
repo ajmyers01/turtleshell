7e43c4630bf8e244ae9ff889c934e679c40aa553
Alex Myers <alex.myers@enspiresoftware.com>
2016-06-16 00:53:26 -0400

    smtp configuration change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,8 +79,8 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :address => "localhost",
-    :port => 25,
-    :domain => "turtleShell.com",
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => "gmail.com",
   }
 end
