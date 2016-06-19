6813662eac77b9b8137b78ea8cfeb5cbcf16cb11
Alex Myers <alex.myers@enspiresoftware.com>
2016-06-18 20:48:01 -0400

    Fix seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,10 @@
 Rake::Task['user:seed'].invoke
 User.all.each do |u|
-10.times do
+10.times do |i|
 u.tasks.create(name: "write the code",
                 description: "finish writing all the code for this project",
-                completed: false )
+                completed: false,
+                priority: i )
 end
 u.reports.create(
   start_date: Date.today.beginning_of_week,
